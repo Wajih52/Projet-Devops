7a19d2a1413a18bc3e61664e623cb0da32f3775c
YasmineB <86532639+LuciferJass@users.noreply.github.com>
2023-02-25 22:17:16 +0100

    Update KaddemProjectApplication.java

--- src/main/java/tn/esprit/kaddemproject/KaddemProjectApplication.java
+++ src/main/java/tn/esprit/kaddemproject/KaddemProjectApplication.java
@@ -19,7 +19,6 @@ public class KaddemProjectApplication {
     }
     public static void main(String[] args) {
         SpringApplication.run(KaddemProjectApplication.class, args);
-        logger.info("test in jenkins");
     }
 
 }
